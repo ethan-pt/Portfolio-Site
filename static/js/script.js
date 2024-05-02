820a6ebb45a4e7db57d82c1350c7c6751bf5ff0c
ethan-pt <tubbeethan@gmail.com>
2024-05-02 15:54:05 -0700

    toggle offcanvas with nav buttons

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+const offcanvas = new bootstrap.Offcanvas('#offcanvasWithBothOptions');
+const offcanvasLinks = document.getElementsByClassName('offcanvas-link');
+
+for (let i = 0; i < offcanvasLinks.length; i++) {
+	offcanvasLinks[i].addEventListener('click', (event) => {
+		offcanvas.toggle();
+	});
+}
