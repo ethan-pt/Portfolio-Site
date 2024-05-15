33147b71e3ff310c399820b436a3d8feacce2cc7
ethan-pt <tubbeethan@gmail.com>
2024-05-14 17:09:56 -0700

    add contact wrapper to scrolling opacity behavior

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,7 @@
 const profileWrapper = document.getElementById('profile-wrapper');
 const aboutWrapper = document.getElementById('about-wrapper');
 const projectsWrapper = document.getElementById('projects-wrapper');
+const contactWrapper = document.getElementById('contact-wrapper');
 const contentWrapper = document.getElementById('content');
 
 window.addEventListener('scroll', () => {
@@ -11,18 +12,27 @@ window.addEventListener('scroll', () => {
 		profileWrapper.style.opacity = '1';
 		aboutWrapper.style.opacity = '0';
 		projectsWrapper.style.opacity = '0';
+		contactWrapper.style.opacity = '0';
 		contentWrapper.style.backgroundColor = 'rgba(33,45,49,0)';
 
 	} else if (offsetFromTop > (viewHeight * 0.5) && offsetFromTop <= (viewHeight * 1.5)) {
 		profileWrapper.style.opacity = '0';
 		aboutWrapper.style.opacity = '1';
 		projectsWrapper.style.opacity = '0';
+		contactWrapper.style.opacity = '0';
 		contentWrapper.style.backgroundColor = 'rgba(33,45,49,1)';
 
 	} else if (offsetFromTop > (viewHeight * 1.5)) {
 		profileWrapper.style.opacity = '0';
 		aboutWrapper.style.opacity = '0';
 		projectsWrapper.style.opacity = '1';
+		contactWrapper.style.opacity = '0';
+		contentWrapper.style.backgroundColor = 'rgba(33,45,49,1)';
+	} else if () {
+		profileWrapper.style.opacity = '0';
+		aboutWrapper.style.opacity = '0';
+		projectsWrapper.style.opacity = '0';
+		contactWrapper.style.opacity = '1';
 		contentWrapper.style.backgroundColor = 'rgba(33,45,49,1)';
 	}
 });
